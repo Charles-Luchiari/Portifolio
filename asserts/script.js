ba3a7e8af58d3975eda58006b52ddd2fc46c31cf
Charles-Luchiari <charles99na@gmail.com>
2023-06-13 08:11:03 -0300

    atualizando

--- asserts/script.js
+++ asserts/script.js
@@ -2,6 +2,11 @@ let nome = window.document.getElementById('nome')
 let email = document.querySelector('#email')
 let assunto = document.querySelector('#assunto')
 
+let nomeOk = false;
+let emailOk = false;
+let mensagemOk = false;
+let cepOk = false;
+
 function validaNome() {
     let txtNome = document.querySelector('#txtNome')
     if (nome.value.length < 3) {
@@ -23,5 +28,75 @@ function validaEmail() {
     } else {
         txtEmail.innerHTML = 'E-mail Válido'
         txtEmail.style.color = 'green'
+    }
+}
+
+function validarMensagem() {
+    let txtMensagem = document.querySelector("#txtMensagem");
+
+    if (mensagem.value.length >= 50) {
+        txtMensagem.innerHTML = "Mensagem muito grande!";
+        txtMensagem.style.color = "red";
+        mensagemOk = false;
+    } else {
+        txtMensagem.innerHTML = "✔";
+        txtMensagem.style.color = "green";
+        mensagemOk = true;
+    }
+}
+
+function enviarForm() {
+    if (nomeOk === true && emailOk === true && mensagemOk === true) {
+        alert(nome.value + ", obrigado pelo contato, aguarde nosso retorno.");
+    } else {
+        alert("Por favor, preencha todos os campos corretamente.");
+    }
+}
+
+const eNumero = (numero) => /^[0-9]+$/.test(numero);
+
+const cepValido = (cep) => cep.length == 8 && eNumero(cep);
+
+function validarCep() {
+    const cep = document.getElementById("cep").value.replace("-", "");
+
+    if (cepValido(cep)) {
+        txtCep.innerHTML = "✔";
+        txtCep.style.color = "green";
+        cepOk = true;
+    } else {
+        txtCep.innerHTML = "CEP Inválido";
+        txtCep.style.color = "red";
+        cepOK = false;
+    }
+}
+
+function consultarCep() {
+    document.getElementById("dados").innerHTML = "";
+    console.log(cepOk);
+    const cep = document.getElementById("cep").value.replace("-", "");
+    const url = `https://viacep.com.br/ws/${cep}/json/`;
+
+    if (cepOk === true || cepValido(cep)) {
+
+        fetch(url)
+            .then((response) => response.json())
+            .then((jsonBody) => {
+                if (jsonBody.erro === true || jsonBody.logradouro === undefined) {
+                    alert("CEP nao encontrado!");
+                } else {
+                    document.getElementById("dados").innerHTML =
+                        jsonBody.logradouro +
+                        "\n" +
+                        jsonBody.bairro +
+                        "\n" +
+                        jsonBody.localidade +
+                        "\n" +
+                        jsonBody.uf;
+                }
+            })
+            .catch((error) => {
+                alert("CEP não encontrado!");
+            });
     }
 }
